dccaa83a35c1e24fdf6b2d61609a38935aff42d0
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-05-24 23:15:04 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,20 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <h1>トップページ</h1>
+      <h1 className="font-bold">トップページ</h1>
+
+      <p className="mt-6 text-gray-500">
+        サイドバーからページ遷移を行い、どのルートが適用されるのか左のツリーで探ってみてください。
+      </p>
+
+      <p className="mt-6 underline text-indigo-600">
+        <a
+          href="https://github.com/dninomiya/nextjs-play-app-routing"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </p>
     </main>
   );
 }
