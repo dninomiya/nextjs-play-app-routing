ac576f78dfcf0e7ec15885a534d9267e92ba8a4d
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-05-25 21:49:36 +0900

    demo

--- app/_components/code.tsx
+++ app/_components/code.tsx
@@ -1,19 +1,21 @@
-import * as fs from 'fs';
-import * as path from 'path';
+// import * as fs from 'fs';
+// import * as path from 'path';
 import CodeView from './code-view';
 
 export default async function CodePanel() {
-  const pathes = ['app/layout.tsx'];
-  const codes = await Promise.all(
-    pathes.map((filePath) => {
-      const jsonPath = path.join(process.cwd(), filePath);
-      return fs.readFileSync(jsonPath, 'utf8');
-    })
-  );
-  const sources = pathes.reduce((acc, path, index) => {
-    acc[path] = codes[index];
-    return acc;
-  }, {} as { [key in string]: string });
+  // const pathes = ['app/layout.tsx'];
+  // const codes = await Promise.all(
+  //   pathes.map((filePath) => {
+  //     const jsonPath = path.join(process.cwd(), filePath);
+  //     return fs.readFileSync(jsonPath, 'utf8');
+  //   })
+  // );
+  // const sources = pathes.reduce((acc, path, index) => {
+  //   acc[path] = codes[index];
+  //   return acc;
+  // }, {} as { [key in string]: string });
 
-  return <CodeView sources={sources} />;
+  // return <CodeView sources={sources} />;
+
+  return <CodeView sources={{}} />;
 }
--- app/_components/core-layout.tsx
+++ app/_components/core-layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header';
 import FileTree from './tree';
+import CodePanel from './code';
 
 export default function CoreLayout({
   children,
@@ -14,6 +15,8 @@ export default function CoreLayout({
         <Header />
         <div className="flex flex-col lg:flex-row gap-10 mt-10">{children}</div>
       </div>
+      {/* @ts-expect-error */}
+      <CodePanel />
     </div>
   );
 }
